ee3ea008b400f6fbf227a88a76fb6217df23496a
Owen Jones <owen@owenjones.net>
2013-09-08 01:09:11 +0100

    Fixed problems with colours

--- luasnake/section.lua
+++ luasnake/section.lua
@@ -1,3 +1,25 @@
 -- section.lua
 
-local Section = {} ; Section.__index = Section
+local section = {} ; section.__index = section
+
+function section.new(parent)
+	local s = setmetatable({parent = parent}, section)
+	return s
+end
+
+function section:set(x, y)
+
+end
+
+function section:nextMove()
+	x, y = section.parent.x, section.parent.y
+end
+
+function section:draw(x, y, size)
+	local r, g, b, a = love.graphics.getColor()
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.rectangle("fill", (x*size)+4, (y*size)+4, size, size)
+	love.graphics.setColor(r, g, b, a)
+end
+
+return section
