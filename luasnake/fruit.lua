2f15fb82d97504a0de8654abd7f08174aeb0a3e4
Owen Jones <owen@owenjones.net>
2013-09-08 01:08:45 +0100

    Added fruit model and drawing method

--- luasnake/fruit.lua
+++ luasnake/fruit.lua
@@ -1,1 +1,21 @@
 -- fruit.lua
+
+local fruit = {} ; fruit.__index = fruit
+
+function fruit.new()
+	local f = setmetatable({}, fruit)
+	return f
+end
+
+function fruit:set(x, y)
+	return
+end
+
+function fruit:draw(x, y, size)
+	local r, g, b, a = love.graphics.getColor()
+	love.graphics.setColor(255, 0, 0)
+	love.graphics.circle("fill", (x*size)+(size/2)+4, (y*size)+(size/2)+4, (size/2), 100)
+	love.graphics.setColor(r, g, b, a)
+end
+
+return fruit
