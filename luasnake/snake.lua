7bb2fd62ec700e2744aaa00ab3ccd04e1433ad25
Owen Jones <owen@owenjones.net>
2013-09-09 01:56:59 +0100

    Tidied directional changes code

--- luasnake/snake.lua
+++ luasnake/snake.lua
@@ -5,36 +5,29 @@ local snake = {} ; snake.__index = snake
 
 function snake.new()
 	local s = setmetatable({
-			x = 0,
-			y = 0,
-			direction = 1,
-			nextDirection = 1,
-			head = false,
-			tail = false,
-		}, snake)
+		x = 0,
+		y = 0,
+		direction = 2,
+		nextDirection = 2,
+		head = false,
+		tail = false,
+	}, snake)
 
 	s:make()
 	return s
 end
 
 function snake:make()
-	self.head = Section.new(false)
+	self.head = Section.new(false, 1)
 	self.head:set(0, 0)
 	self.tail = self.head
 end
 
 function snake:move()
 	local x, y = self.x, self.y
-
-	if self.direction == 0 then
-		y = y - 1
-	elseif self.direction == 1 then
-		x = x + 1
-	elseif self.direction == 2 then
-		y = y + 1
-	elseif self.direction == 3 then
-		x = x - 1
-	end
+	local change = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	x = x + change[self.direction][1]
+	y = y + change[self.direction][2]
 
 	-- Wall wrap-around if in game modes 1 & 2
 	if game.mode > 1 then
@@ -60,7 +53,8 @@ end
 
 function snake:extend(num)
 	for i = 1, num do
-		local s = Section.new(self.tail)
+		local id = game.sections + i
+		local s = Section.new(self.tail, id)
 		self.tail = s
 	end
 	game.sections = game.sections + num
