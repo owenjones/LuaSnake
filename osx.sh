815749dbdc562bfcd2acd84c0ef51e5264077990
= <owen@owenjones.net>
2013-12-07 19:49:57 +0000

    Now removes .DS_Store files when zipping game files

--- osx.sh
+++ osx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Runs LuaSnake on OS X (only tested on 10.8)
-cd luasnake && zip -r ../luasnake.love *
+cd luasnake && rm -rf */.DS_Store && zip -r ../luasnake.love *
 exec /Applications/love.app/Contents/MacOS/love ../luasnake.love
