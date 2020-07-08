977bb4ef2308b033d58ab3621e425997eb979554
Owen Jones <owen@owenjones.net>
2020-07-08 18:46:54 +0100

    have checked this still works on at least 10.11

--- osx.sh
+++ osx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Runs LuaSnake on OS X (only tested on 10.8)
+# Runs LuaSnake on OS X (tested up to 10.11)
 cd luasnake && rm -rf */.DS_Store && zip -r ../luasnake.love *
 exec /Applications/love.app/Contents/MacOS/love ../luasnake.love
